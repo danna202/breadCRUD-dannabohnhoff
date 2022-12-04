909df43852f929c440a6ca557c775f066a793fdd
Danna Bohnhoff <dannab101@gmail.com>
2022-12-04 09:31:01 -0500

    3 12-4-22

---
+++ views/show.jsx
@@ -0,0 +1,15 @@
+const React = require('react') const
+Default = require('./layouts/default')
+
+function Show ({bread}) {
+    console.log(bread.name)
+        return (
+         <Default>
+             <h2>Show Page</h2>
+         </Default>
+
+        )
+}
+
+module.exports = Show
+    
